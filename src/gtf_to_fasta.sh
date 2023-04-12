0cb18d0e7401b0dad4e41f6834a7527c1b9ffdd1
Tyrone Chen <tyrone.chen@monash.edu>
2023-04-12 21:11:58 +1000

    Allow removing overlapping regions for upstream seqs

--- src/gtf_to_fasta.sh
+++ src/gtf_to_fasta.sh
@@ -13,11 +13,11 @@ for d in "${DEPENDENCIES[@]}"; do
 done
 
 # we expect fasta/gz, gtf and coords in integer format
-if [ "$#" -eq 3 ] || [ "$#" -eq 4 ] ; then
+if [ "$#" -eq 3 ] || [ "$#" -eq 4 ] || [ "$#" -eq 5 ]; then
   # correct number of arguments so we carry on
   echo "" > /dev/null
 else
-  echo "Usage: $0 <genome.fasta> <annotation.gtf> <length_upstream_of_tss> [cpu]"
+  echo "Usage: $0 <genome.fasta> <annotation.gtf> <length_upstream_of_tss> [cpu] [--keep_overlaps]"
   echo "Extract a range of sequences upstream of the TSS, writeout as a bed file"
   exit 1
 fi
@@ -26,9 +26,12 @@ fi
 genome=$1
 # annotation gtf file
 i=$2
-# length upstream of TSS
+# length from TSS (if -ve takes upstream seqs)
 len=$3
+# number of cpus needed
 cpu=$4
+# discard overlapping regions by default
+keep_overlaps=$5
 
 # if cpu not provided set to 1
 if [ -z "$cpu" ]; then
@@ -48,7 +51,7 @@ if (( $len < 0 )); then
   len_name=${len}
   gtf2bed < ${i} | \
     awk -v len=${len_real} -F'\t' '{if($2<=len) next; if($2) print}' | \
-    awk -v len=${len_real} -F'\t' 'BEGIN{OFS="\t"} { $3=$2; $2=$2-len ; print }' > \
+    awk -v len=${len_real} -F'\t' 'BEGIN{OFS="\t"} { $3=$2-1; $2=$2-len-1 ; print }' > \
     ${i}.${len_name}.bed.tmp
 else
   len_real=${len}
@@ -58,20 +61,33 @@ else
     ${i}.${len_name}.bed.tmp
 fi
 
+# NOTE: REF ONLY DO NOT USE, REDUNDANT WITH ABOVE
 # offset from TSS to mine for information-rich regions
-echo bedops --range -${len_real}:-${len_real} --everything ${i}.${len_name}.bed.tmp \
-  ${i}.${len_name}.bed
-bedops --range -${len_real}:-${len_real} --everything ${i}.${len_name}.bed.tmp > \
-  ${i}.${len_name}.bed
+# echo bedops --range -${len_real}:-${len_real} --everything ${i}.${len_name}.bed.tmp \
+#   ${i}.${len_name}.bed
+# bedops --range -${len_real}:-${len_real} --everything ${i}.${len_name}.bed.tmp > \
+#   ${i}.${len_name}.bed
 
 if (( $len < 0 )); then
   # it is possible that this may overlap into genic regions but informative
   # specifically, we are only interested in the overlaps for the -ve (upstream) case
-  echo bedops --element-of 1 ${i}.${len_name}.bed.tmp ${i}.${len_name}.bed \
+  # if we want to do this, we need the original coordinates of all genes
+  echo gtf2bed ${i} ${i}.bed.tmp
+  gtf2bed < ${i} > ${i}.bed.tmp
+  echo bedops --element-of 1 ${i}.${len_name}.bed.tmp ${i}.bed.tmp \
     ${i}.${len_name}.bed.overlap
-  bedops --element-of 1 ${i}.${len_name}.bed.tmp ${i}.${len_name}.bed > \
+  bedops --element-of 1 ${i}.${len_name}.bed.tmp ${i}.bed.tmp > \
     ${i}.${len_name}.bed.overlap
-  wc -l ${i} ${i}.${len_name}.bed ${i}.${len_name}.bed.overlap
+  wc -l ${i}.bed.tmp ${i}.${len_name}.bed.tmp ${i}.${len_name}.bed.overlap
+  if [[ $keep_overlaps -eq '--keep_overlaps' ]]; then
+    echo "Retain overlaps in file"
+  else
+    echo "Remove overlaps in file, final count of non-overlapping regions:"
+    bedtools subtract -A -a ${i}.${len_name}.bed.tmp -b ${i}.${len_name}.bed.overlap > \
+      ${i}.${len_name}.bed.tmp.tmp
+    mv ${i}.${len_name}.bed.tmp.tmp ${i}.${len_name}.bed.tmp
+    wc -l ${i}.${len_name}.bed.tmp
+  fi
 else
   echo "Extracting downstream from TSS, gene overlap stats not relevant"
 fi
